cb2887ddd0dac5a24f831d6657cd96fe1fa8a339
Larry Baltz <larry@baltz.org>
2013-08-29 22:10:00 +0200

    Report the location the pill was created

--- lib/poison_pill.rb
+++ lib/poison_pill.rb
@@ -5,11 +5,26 @@ end
 class PoisonPill < BasicObject
   class NotFulfilledError < ::StandardError; end
   def initialize(container, attr_name)
+    @caller = ::Kernel.caller
+    @called_line, @caller_line = @caller[0..1]
     @container = container
     @attr_name = attr_name
   end
 
-  def __posion?; true; end
+  def __poison?; true; end
+  def __origin__; @origin; end
+
+  def __called_pathname__
+    @called_pathname || __decode_called__.first
+  end
+
+  def __called_line_number__
+    @called_line_number || __decode_called__[1]
+  end
+
+  def __caller_method_name__
+    @caller_method_name ||= @caller_line[/:in\s+`(.*)'$/, 1].to_sym
+  end
 
   def method_missing(method_name, *params, &block)
     message = "The attribute, #{@attr_name}, in #{@container.inspect} was not filled-in"
@@ -26,6 +41,18 @@ class PoisonPill < BasicObject
     method_call = "#{method_name}(#{params.map(&:inspect).join(', ')})"
     message += " [triggered with call to #{method_call}]"
 
+    message += "\n #{__called_pathname__}:#{__called_line_number__} in ##{__caller_method_name__}"
+
     ::Kernel.raise NotFulfilledError, message
   end
+
+  private
+
+  def __decode_called__
+    f, l = @called_line.match(/^(.*?):(\d+)/).captures
+    @called_pathname = f
+    @called_line_number = l.to_i
+    [@called_pathname, @called_line_number]
+  end
+
 end
--- spec/lib/poison_pill_spec.rb
+++ spec/lib/poison_pill_spec.rb
@@ -1,6 +1,22 @@
+require 'spec_helper'
+
+describe Object do
+  it '#__poison?' do
+    Object.new.__poison?.should be_false
+  end
+end
+
 describe PoisonPill do
   let(:container) { "container" }
-  let(:pill) { PoisonPill.new(container, :some_attribute) }
+
+  def calling_method
+    @file = __FILE__; @line_number = __LINE__
+    @pill = PoisonPill.new(container, :some_attribute)
+  end
+
+  before do
+    calling_method
+  end
 
   it 'can be instantiated with an object and attribute name' do
     expect {
@@ -8,8 +24,12 @@ describe PoisonPill do
     }.to_not raise_error
   end
 
+  it '#__poison?' do
+    @pill.__poison?.should be_true
+  end
+
   it 'raises a simple error message if the container does not implment #fulfilled?' do
-    expect{ pill.some_method }.to raise_error(
+    expect{ @pill.some_method }.to raise_error(
       PoisonPill::NotFulfilledError,
       /^The attribute, some_attribute, in "container" was not filled-in/
     )
@@ -17,7 +37,7 @@ describe PoisonPill do
 
   it 'raises a error with an un-fulfilled container message if container un-fulfilled' do
     stub(container).fulfilled?{ false }
-    expect{ pill.some_method }.to raise_error(
+    expect{ @pill.some_method }.to raise_error(
       PoisonPill::NotFulfilledError,
       /some_attribute.*"container".*because the associated REST call has not been completed/
     )
@@ -25,10 +45,49 @@ describe PoisonPill do
 
   it 'raises a error with an fulfilled container message if container is fulfilled' do
     stub(container).fulfilled?{ true }
-    expect{ pill.some_method }.to raise_error(
+    expect{ @pill.some_method }.to raise_error(
       PoisonPill::NotFulfilledError,
       /some_attribute.*"container".*even though the associated REST call was completed/
     )
+  end
+
+  it 'raises a error with the called pathname' do
+    expect{ @pill.some_method }.to raise_error(
+      PoisonPill::NotFulfilledError,
+      /#{@file}/
+    )
+  end
+
+  it 'raises a error with the called line number' do
+    expect{ @pill.some_method }.to raise_error(
+      PoisonPill::NotFulfilledError,
+      /#{@line_number+1}/
+    )
+  end
+
+  it 'raises a error with the caller method' do
+    expect{ @pill.some_method }.to raise_error(
+      PoisonPill::NotFulfilledError,
+      /#{@calling_method}/
+    )
+  end
+
+  describe '#__called_pathname__' do
+    it 'returns file where pill was filled in' do
+      @pill.__called_pathname__.should == @file
+    end
+  end
+
+  describe '#__called_line_number__' do
+    it 'returns line where pill was filled in' do
+      @pill.__called_line_number__.should == @line_number + 1
+    end
+  end
+
+  describe '#__caller_method_name' do
+    it 'returns caller method' do
+      @pill.__caller_method_name__.should == :calling_method
+    end
   end
 end
 
